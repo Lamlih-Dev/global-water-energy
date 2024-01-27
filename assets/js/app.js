d57601c5652217de7e9a07c0e1c9334324814228
Lamlih-Dev <contact.ahmed.lamlih@gmail.com>
2024-01-27 05:03:35 +0100

    Enhance the alert message and box.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,19 @@
 AOS.init();
 var upperNavProductsButton = document.getElementById("upperProductsButton");
 var lowerNavProductsButton = document.getElementById("lowerProductsButton");
+var alert = document.getElementById("alert");
+var btnCloseAlert = document.getElementById("btnCloseAlert");
 var mainButton = document.getElementById("mainButton");
 var productsSectionPosition = document.getElementById('products').getBoundingClientRect().top - 50;
 
+btnCloseAlert.addEventListener("click",()=>{
+    sessionStorage.setItem("alertClosed", "true");
+})
+window.addEventListener("load",()=>{
+    if(sessionStorage.getItem('alertClosed') !== null){
+        alert.style.display = "none";
+    }
+})
 upperNavProductsButton.addEventListener("click", (e)=>{
     e.preventDefault();
     window.scrollTo(0,productsSectionPosition);
